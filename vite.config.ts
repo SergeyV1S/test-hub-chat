d5a6583c9a8c0a294ab44f1462b784deda6cb219
Sergey Yatsuk <v1s.coop@yandex.ru>
2024-12-26 22:44:29 +0500

    fix aliases

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,9 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@app": path.resolve(__dirname, "./src/app"),
-      "@modules": path.resolve(__dirname, "./src/modules"),
-      "@shared": path.resolve(__dirname, "./src/shared")
+      "@app": path.resolve(__dirname, "src/app"),
+      "@modules": path.resolve(__dirname, "src/modules"),
+      "@shared": path.resolve(__dirname, "src/shared")
     }
   },
   define: {
