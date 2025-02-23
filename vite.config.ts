77e227962fffdd2f88d472fc8f0495626d9579ba
Sergey Yatsuk <v1s.coop@yandex.ru>
2025-02-23 22:21:36 +0500

    fix html & add pwa feat

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,30 @@ import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Igniz AI",
+        short_name: "Igniz AI",
+        description: "Igniz AI",
+        theme_color: "#ffffff",
+        lang: "ru",
+        start_url: "/",
+        icons: [
+          { purpose: "maskable", sizes: "512x512", src: "icon512_maskable.png", type: "image/png" },
+          { purpose: "any", sizes: "512x512", src: "icon512_rounded.png", type: "image/png" }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src")
